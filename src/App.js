6f53e6731a542beebda3c278ba124dd7cb610ea9
German <german.a.almonte@gmail.com>
2022-02-17 23:59:25 -0500

    first commit, finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,91 @@
-import './App.css';
-import Nav from './components/Nav'
-import ContentWrapper from './components/ContentWrapper'
-import Footer from './components/Footer'
+import { useEffect } from "react";
+import "./App.css";
+import { useSelector, useDispatch, connect } from "react-redux";
+import {
+  fetchData,
+  incrementId,
+  decrementId,
+  enterId,
+  clearData,
+} from "./features/dataSlice";
+
+const mapStateToProps = (state) => ({
+  objectId: state.data.objectId,
+});
 
 function App() {
-  
+  const data = useSelector((state) => state.data);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [data.objectId, dispatch]);
+
+  const artData = () => {
+    if (data.apiData) {
+      return (
+        <img
+          style={{
+            width: "100vw",
+            height: "100vh",
+            objectFit: "scale-down",
+          }}
+          src={data.apiData.primaryImage}
+          alt={data.apiData.title}
+        />
+      );
+    } else {
+      return <h1>Image Not Found</h1>;
+    }
+  };
+
   return (
-    <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
-      <Nav />
-      <ContentWrapper />
-      <Footer />
+    <div className="App">
+      <div>
+        <button
+          onClick={() => {
+            dispatch(fetchData());
+          }}
+        >
+          Trigger Thunk
+        </button>
+        <button
+          onClick={() => {
+            dispatch(clearData());
+          }}
+        >
+          Clear
+        </button>
+        <button
+          onClick={() => {
+            dispatch(incrementId());
+          }}
+        >
+          Next
+        </button>
+        <button
+          onClick={() => {
+            dispatch(decrementId());
+          }}
+        >
+          Back
+        </button>
+      </div>
+      <input
+        type="number"
+        value={data.objectId}
+        onChange={(e) => {
+          dispatch(enterId(e.target.value));
+        }}
+      />
+      <div>{data.apiData.title}</div>
+      <div>Date: {data.apiData.objectDate}</div>
+      <div>{data.apiData.creditLine}</div>
+      <div>{data.apiData.repository}</div>
+
+      {artData()}
     </div>
   );
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
